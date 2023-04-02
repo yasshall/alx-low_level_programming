8572b71f85d355dc37ace00930a74cbb79c778ce
Yasser hallou <peekmatecom@gmail.com>
2023-04-02 00:26:03 -0700

    task 10 fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,22 +1,23 @@
 #include "main.h"
-
 /**
-  * _strcpy - Copy a string
-  * @dest: Destination value
-  * @src: Source value
-  *
-  * Return: the pointer to dest
-  */
-char *_strcpy(char *dest, char *src)
+ * _atoi - convert a string to an integer.
+ * @s: pointer to string
+ *
+ * Return: integer value of string, or 0 if no digits are found
+ */
+int _atoi(char *s)
 {
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	dest[i++] = '\0';
-
-	return (dest);
+int sign = 1;
+int result = 0;
+while (*s)
+{
+if (*s == '-')
+sign = -sign;
+else if (*s >= '0' && *s <= '9')
+result = result * 10 + (*s - '0');
+else if (result > 0)
+break;
+s++;
+}
+return (sign *result);
 }
